3cfa62ec3faa8b971167b3934dc8b5382cb5b2fb
Giacomo Camerano <giacomo.camerano@gmail.com>
2025-01-04 17:28:02 +0100

    feat: add category page

---
+++ app/category/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import { BlogCard } from '@/components/Card'
+import { GetCategories, GetCategoryPost } from '@/data'
+import type { Post } from '@/types'
+import type { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+
+export async function generateMetadata({
+    params
+}: {
+    params: Promise<{ slug: string }>
+}): Promise<Metadata> {
+    const { slug } = await params
+    return {
+        title: `Articles Categorised as ${slug?.trim().replaceAll(' ', '-')}`
+    }
+}
+
+export async function generateStaticParams() {
+    return GetCategories()
+}
+
+export default async function Page({
+    params
+}: {
+    params: Promise<{ slug: string }>
+}) {
+    const slug = (await params).slug
+    const posts = await GetCategoryPost(slug)
+    if (posts.length === 0) {
+        notFound()
+    }
+    return (
+        <>
+            <div className="container mx-auto my-24 px-4">
+                <h2 className="my-8 text-3xl font-bold capitalize">
+                    Articles Categorised as {slug.replaceAll('-', ' ')}
+                </h2>
+            </div>
+
+            <div className="container mx-auto mt-8">
+                {posts.map((item: Post) => (
+                    <BlogCard item={item} key={item.id} />
+                ))}
+            </div>
+        </>
+    )
+}
