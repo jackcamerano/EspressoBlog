66d419640fa71be37aacccaddc517a487cd2d5a0
Giacomo Camerano <giacomo.camerano@gmail.com>
2025-01-06 11:56:35 +0100

    chore: update dependencies, fix broken

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
-const postCss = {
+module.exports = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
         ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
     }
 }
-
-export default postCss
