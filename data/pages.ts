9fa5fe1fefb5df9373212d833e20de36f331605c
Giacomo Camerano <giacomo.camerano@gmail.com>
2025-01-20 08:50:46 +0100

    chore: add sample json data

--- data/pages.ts
+++ data/pages.ts
@@ -1,3 +1,52 @@
 import { Page } from '@/types/types'
 
-export const pages: Page[] = []
+export const pages: Page[] = [
+    {
+        title: 'About',
+        description: 'Brief description of the site',
+
+        featuredImage: {
+            name: 'image name',
+            url: 'https://dummyimage.com/1280x853/000/fff',
+            alternativeText:
+                'Alt text for the image. This is a description of the image.'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'about',
+        date: '2025-01-15',
+        content:
+            '## Investment build early group actually despite.\n\nManagement information voice alone. Million question different build. Agent one turn care believe capital however.\n\n1. herself\n2. fund\n\n**Answer agreement professor purpose it.**'
+    },
+    {
+        title: 'Contact',
+        description: 'Contact page description',
+        featuredImage: {
+            url: 'https://dummyimage.com/1280x853/000/fff',
+            alternativeText:
+                'Alt text for the image. This is a description of the image.'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'contact',
+        date: '2025-01-12',
+        content:
+            '## Education eat ago society office talk.\n\nTeach eat official lead third. Task either difficult national. Single black sort officer outside. Form since too develop community state thing fast. Line require baby fine. Change local collection see.\n\n1. me\n2. popular\n\n**Up series senior field responsibility goal might.**'
+    },
+    {
+        title: 'Support',
+        description: 'Another random page description',
+        featuredImage: {
+            url: 'https://dummyimage.com/1280x853/000/fff'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'support',
+        date: '2025-01-10',
+        content:
+            '## Wind foreign strong these station.\n\nJob receive me a. Fast low home house for hand. Style color new alone real debate attention brother. Answer leg research approach administration star. Few him today see under before.\n\n1. study\n2. top\n\n**Agent rest case capital page.**'
+    }
+]
--- data/posts.ts
+++ data/posts.ts
@@ -1,3 +1,378 @@
 import { Post } from '@/types/types'
 
-export const posts: Post[] = []
+export const posts: Post[] = [
+    {
+        title: 'Hand run responsibility',
+        description: 'Get people serious small couple garden bill',
+        tags: [
+            {
+                name: 'Paper',
+                slug: 'paper'
+            },
+            {
+                name: 'Why',
+                slug: 'why'
+            },
+            {
+                name: 'Member',
+                slug: 'member'
+            }
+        ],
+        categories: [
+            {
+                name: 'News',
+                slug: 'news'
+            },
+            {
+                name: 'Environmental',
+                slug: 'environmental'
+            }
+        ],
+        featuredImage: {
+            url: 'https://dummyimage.com/1280x853/000/fff',
+            alternativeText: 'Policy smile eight person feeling PM year late'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'hand-run-responsibility',
+        date: '2025-01-13T15:24:37',
+        content:
+            '## Culture thing boy boy area successful.\n\nWear maybe your force order. Performance special federal contain learn. Water couple office may. Hotel around land first son carry set. Would knowledge instead hard letter thing ago.\n\n- sell\n- police\n\n> Call again letter.'
+    },
+    {
+        title: 'Policy author concern wait big',
+        description: 'Enjoy before what wear ask sense.',
+        tags: [
+            {
+                name: 'Paper',
+                slug: 'paper'
+            },
+            {
+                name: 'Step',
+                slug: 'step'
+            }
+        ],
+        categories: [
+            {
+                name: 'News',
+                slug: 'news'
+            },
+            {
+                name: 'Environmental',
+                slug: 'environmental'
+            }
+        ],
+        featuredImage: {
+            name: 'anyone',
+            url: 'https://dummyimage.com/1280x853/000/fff'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'policy-author-concern-wait-big',
+        date: '2025-01-11T17:26:37',
+        content:
+            '## Indicate second how small.\n\nScene realize how onto. Smile expect food work but. Generation dinner federal suffer expert forward write. Red view follow imagine something. Mean hot without wide fund public. Before information office Congress or front.\n\n- and\n- star\n\n> Range control protect office.'
+    },
+    {
+        title: 'Against tonight begin show huge',
+        description: 'Relationship floor current everybody response.',
+        tags: [
+            {
+                name: 'Paper',
+                slug: 'paper'
+            },
+            {
+                name: 'Keep',
+                slug: 'keep'
+            },
+            {
+                name: 'Why',
+                slug: 'why'
+            }
+        ],
+        categories: [
+            {
+                name: 'Realize',
+                slug: 'realize'
+            }
+        ],
+        featuredImage: {
+            name: 'thought',
+            url: 'https://dummyimage.com/1280x853/000/fff',
+            alternativeText: 'Find or lawyer enter read hour take.'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'against-tonight-begin-show-huge',
+        date: '2025-01-14T06:48:24',
+        content:
+            '## Ahead stay cultural what option.\n\nCommon fire yeah reflect must window. Part involve him they economic late serve discover. Book threat kid trial. Some election simply store sometimes. Administration behind like bring speech low. Science generation may agree watch point my. Although though say.\n\n- power\n- response\n\n> Where available right watch.'
+    },
+    {
+        title: 'American worker green food history meet present',
+        description: 'Six yourself table.',
+        tags: [
+            {
+                name: 'Paper',
+                slug: 'paper'
+            },
+            {
+                name: 'Keep',
+                slug: 'keep'
+            }
+        ],
+        categories: [
+            {
+                name: 'Environmental',
+                slug: 'environmental'
+            },
+            {
+                name: 'Realize',
+                slug: 'realize'
+            }
+        ],
+        featuredImage: {
+            name: 'how',
+            url: 'https://dummyimage.com/1280x853/000/fff'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'american-worker-green-food-history-meet-present',
+        date: '2025-01-07T12:46:30',
+        content:
+            '## Read level its late home building heart simply.\n\nDeep space sign among tonight food able yeah. They store role move. Quality me score all suggest production much.\n\n- almost\n- wrong\n\n> Rather religious billion model writer floor write.'
+    },
+    {
+        title: 'Whatever tree yourself administration difference language',
+        description: 'Station better practice market.',
+        tags: [
+            {
+                name: 'Why',
+                slug: 'why'
+            },
+            {
+                name: 'Success',
+                slug: 'success'
+            }
+        ],
+        categories: [
+            {
+                name: 'Realize',
+                slug: 'realize'
+            }
+        ],
+        featuredImage: {
+            name: 'because',
+            url: 'https://dummyimage.com/1280x853/000/fff'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'whatever-tree-yourself-administration-difference-language',
+        date: '2025-01-08T23:48:04',
+        content:
+            '## Law discussion go rather know standard.\n\nHave trade deal now key although. Environmental sport director during institution. Whose fine head such simple himself.\n\n- agency\n- rule\n\n> Event Democrat society ok value operation.'
+    },
+    {
+        title: 'Figure ground section long white responsibility',
+        description:
+            'Look degree find song performance relate physical positive.',
+        tags: [
+            {
+                name: 'Success',
+                slug: 'success'
+            },
+            {
+                name: 'Keep',
+                slug: 'keep'
+            },
+            {
+                name: 'Step',
+                slug: 'step'
+            }
+        ],
+        categories: [
+            {
+                name: 'News',
+                slug: 'news'
+            },
+            {
+                name: 'Environmental',
+                slug: 'environmental'
+            }
+        ],
+        featuredImage: {
+            name: 'student',
+            url: 'https://dummyimage.com/1280x853/000/fff',
+            alternativeText: 'Six trade father.'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'figure-ground-section-long-white-responsibility',
+        date: '2025-01-16T20:06:05',
+        content:
+            '## Record send international participant other character.\n\nBody every director matter best. Up method society coach generation among. Per again last certainly challenge wall us over. Seat even oil morning.\n\n- letter\n- sister\n\n> Generation worker build example tell food area.'
+    },
+    {
+        title: 'Trade position beyond need party',
+        description: 'Style alone baby still',
+        tags: [
+            {
+                name: 'Such',
+                slug: 'such'
+            },
+            {
+                name: 'Hour',
+                slug: 'hour'
+            },
+            {
+                name: 'Step',
+                slug: 'step'
+            }
+        ],
+        categories: [
+            {
+                name: 'Realize',
+                slug: 'realize'
+            },
+            {
+                name: 'News',
+                slug: 'news'
+            }
+        ],
+        featuredImage: {
+            name: 'find',
+            url: 'https://dummyimage.com/1280x853/000/fff'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'trade-position-beyond-need-party',
+        date: '2025-01-11T20:57:42',
+        content:
+            '## Book election them why product bad significant.\n\nProbably easy food effort other detail. Than detail start morning change to. Prepare travel become individual she between kitchen dream.\n\n- final\n- method\n\n> Friend step itself college security.'
+    },
+    {
+        title: 'Management box throw area dream issue',
+        description: 'Marriage tonight painting contain',
+        tags: [
+            {
+                name: 'Why',
+                slug: 'why'
+            },
+            {
+                name: 'Upon',
+                slug: 'upon'
+            },
+            {
+                name: 'Paper',
+                slug: 'paper'
+            }
+        ],
+        categories: [
+            {
+                name: 'News',
+                slug: 'news'
+            },
+            {
+                name: 'Environmental',
+                slug: 'environmental'
+            }
+        ],
+        featuredImage: {
+            name: 'billion',
+            url: 'https://dummyimage.com/1280x853/000/fff',
+            alternativeText: 'Case card computer.'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'management-box-throw-area-dream-issue',
+        date: '2025-01-18T02:18:37',
+        content:
+            '## Part artist air write your customer still study.\n\nSit power race. Or Congress remain TV. Mother outside bill some.\n\n- alone\n- vote\n\n> Wish single may rate.'
+    },
+    {
+        title: 'Believe part far',
+        description: 'Before mission view next letter night.',
+        tags: [
+            {
+                name: 'Paper',
+                slug: 'paper'
+            },
+            {
+                name: 'Nice',
+                slug: 'nice'
+            },
+            {
+                name: 'Success',
+                slug: 'success'
+            }
+        ],
+        categories: [
+            {
+                name: 'Interview',
+                slug: 'interview'
+            },
+            {
+                name: 'Realize',
+                slug: 'realize'
+            }
+        ],
+        featuredImage: {
+            name: 'both',
+            url: 'https://dummyimage.com/1280x853/000/fff'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'believe-part-far',
+        date: '2025-01-10T16:25:25',
+        content:
+            '## Commercial analysis note thing.\n\nVoice whole standard car Congress everybody. Mrs with think her. Listen arm candidate drop loss. Four wrong hit ready activity others TV kid. Close fast fact anything image.\n\n- brother\n- control\n\n> Can listen account without force join.'
+    },
+    {
+        title: 'Everything far force use start take',
+        description: 'Outside approach image music might',
+        tags: [
+            {
+                name: 'Success',
+                slug: 'success'
+            },
+            {
+                name: 'Nice',
+                slug: 'nice'
+            },
+            {
+                name: 'Member',
+                slug: 'member'
+            }
+        ],
+        categories: [
+            {
+                name: 'News',
+                slug: 'news'
+            },
+            {
+                name: 'Interview',
+                slug: 'interview'
+            }
+        ],
+        featuredImage: {
+            name: 'range',
+            url: 'https://dummyimage.com/1280x853/000/fff'
+        },
+        author: {
+            name: 'Jack'
+        },
+        slug: 'everything-far-force-use-start-take',
+        date: '2025-01-16T07:39:06',
+        content:
+            '## Wrong increase never.\n\nCell word should fly ask measure above. Yard provide term full parent tax. Despite decide next easy together almost able.\n\n- skill\n- any\n\n> Friend executive baby figure whole left owner.'
+    }
+]
